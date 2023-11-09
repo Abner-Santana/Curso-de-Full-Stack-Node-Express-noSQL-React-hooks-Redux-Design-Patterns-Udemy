07841343ee2031c0f39074d5f9a65f8f8154bd31
Abner-Santana <abnersantanadasilva2022+github@gmail.com>
2023-11-09 09:08:20 -0400

    Aula 36

---
+++ Seção 3 - JavaScript - Lógica de Programação/Aula 36/index.js
@@ -0,0 +1,12 @@
+const pessoa = {
+    nome : 'Abner',
+    idade : '22',
+};
+
+//Pesquisa de forma dinamica
+console.log(pessoa['nome']);
+console.log(pessoa.nome);
+
+for(let index in pessoa){
+    console.log(index, pessoa[index]);
+}
